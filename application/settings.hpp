6fea90d89939f3344a94795f593c04c4d8ca6ad7
asifZaman0362 <zero362001@gmail.com>
2020-04-11 11:54:41 +0530

    added default settings

--- application/settings.hpp
+++ application/settings.hpp
@@ -5,6 +5,8 @@
 
 namespace zg {
     struct Settings {
+        // Returns default settings
+        Settings() : WIDTH(800), HEIGHT(640), TITLE("Window"), STYLE(1), FPS_LIMIT(0) {}
         uint16 WIDTH;
         uint16 HEIGHT;
         string TITLE;
